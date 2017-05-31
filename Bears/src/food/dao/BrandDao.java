bf2cc935820a390a096b93bc54cbba141f68fbd4
babysean <tkdgns7070@gmail.com>
2017-05-31 18:21:54 +0900

    Add files via upload

---
+++ Bears/src/food/dao/BrandDao.java
@@ -0,0 +1,26 @@
+package food.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import com.ibatis.sqlmap.client.SqlMapClient;
+import food.util.ServiceUtil;
+import food.vo.StoreVo;
+
+public class BrandDao {
+	private static SqlMapClient sqlMapper;
+	static {
+		sqlMapper = ServiceUtil.getSqlMap();
+	}
+	// select * from store;
+	public static List<StoreVo> selectStore() throws Exception{
+		return sqlMapper.queryForList("store.selectStore");
+	}
+	
+	public static List<StoreVo> searchStore(HashMap<String,Object>map)throws Exception{
+        return sqlMapper.queryForList("store.searchStore",map);
+	}
+	
+	public static int getTotalRow(HashMap<String, Object>map) throws Exception{
+	      return (Integer)sqlMapper.queryForObject("store.getTotalRow", map);
+	   }
+}
